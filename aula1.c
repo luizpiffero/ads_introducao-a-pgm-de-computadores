ea35673b7189c425257345f336fe8b076e817521
Luiz Gustavo Piffero <lgpp18@gmail.com>
2025-03-05 20:25:09 -0300

    progressão da aula
    aula1.c - basico de como instalar IDE e HelloWorld.
    aula2.c - introdução a variáveis

--- aula1.c
+++ aula1.c
@@ -1,10 +1,13 @@
+//Esse comando inclui a biblioteca padrão de entrada e saída.
 #include <stdio.h>
 
+//Aqui é onde o programa começa a ser escrito e começa a funcionar.
 int main(){
 
-    pri
+    //O \n adiciona uma nova linha ao final da mensagem. Tem que colocar também sempre o ; no final de cada linha.
+    printf("Hello World!\n");
 
 
-
-
+    //Avisa ao sistema que o programa terminou com sucesso.
+    return 0;
 }
